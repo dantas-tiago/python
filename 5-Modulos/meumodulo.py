36f7000b053fc71fda3b06bf8fc83d8e7ec5d2b0
Tiago Dantas <tiago.h.dantas@hotmail.com>
2024-04-03 16:14:45 -0300

    correções em escritas e adição de novos exemplos

---
+++ 5-Modulos/meumodulo.py
@@ -0,0 +1,10 @@
+# atributo
+criador = "Tiago"
+
+# Função para calculo da área do quadrado
+def area_quadrado(lado): 
+    return lado * lado
+
+# Função para calculo da área de um triângulo
+def area_triangulo(base, altura):    
+    return (base * altura)/2
